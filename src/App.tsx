c8cf75621c77b0493b7b93416d495860c4e2255e
Davide Cesaroni <davide.cesaroni@gmail.com>
2021-03-10 18:42:40 +0100

    Add fonts and color to theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 const App = () => {
   // Create the count state.
@@ -12,20 +10,16 @@ const App = () => {
   }, [count, setCount]);
   // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </p>
+    <div className="p-16">
+      <header>
+        <h1 className="text-9xl">Big title</h1>
+        <h1>Fonts selected, this is h1</h1>
+        <h2>This, instead, is an h2</h2>
+        <h3>than an h3</h3>
+        <h4>than an h4</h4>
+        <h5>than an h5</h5>
+        <h6>and, in the end, an h6</h6>
+        <p>This is Telex font used for body test</p>
       </header>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: "#FECA2F",
+      },
+      secondary: {
+        DEFAULT: "#FD9ED8",
+      },
+      accent: {
+        DEFAULT: "#01c0c5",
+      },
+      success: "",
+      warning: "",
+      danger: "",
+      neutral: {
+        dark: "#282B3A",
+        medium: "#5D606B",
+        light: "#F6F6F6",
+      },
+    },
+    fontFamily: {
+      head: ["Montserrat", "sans-serif"],
+      subhead: ["Ruda", "sans-serif"],
+      body: ["Telex", "sans-serif"],
+    },
     extend: {},
   },
   variants: {
